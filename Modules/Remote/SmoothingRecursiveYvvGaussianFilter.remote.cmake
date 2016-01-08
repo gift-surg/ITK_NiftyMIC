ea0402e44ec2476ec96c45cfcad5e7d4b5447414
Michael Ebner <michael.ebner.14@ucl.ac.uk>
2016-01-08 12:28:42 +0000

    ENH: YVV-filter remote.cmake-file: Do not use given git-tag.
    Change-Id: I94c9f80df73e0df35472cc5123e1b49d389b7015

--- Modules/Remote/SmoothingRecursiveYvvGaussianFilter.remote.cmake
+++ Modules/Remote/SmoothingRecursiveYvvGaussianFilter.remote.cmake
@@ -1,6 +1,11 @@
 #Contact: I. Vidal-Migallon <irina.vidal-migallon@inria.fr>
-itk_fetch_module(SmoothingRecursiveYvvGaussianFilter
-  "GPU and CPU Young & Van Vliet Recursive Gaussian Smoothing Filter: http://hdl.handle.net/10380/3425"
-  GIT_REPOSITORY ${git_protocol}://github.com/Inria-Asclepios/SmoothingRecursiveYvvGaussianFilter
-  GIT_TAG 69a6d9569c3a160872ba579f96db789df2ef07e6
-  )
+
+# \note (Michael Ebner, 08.01.2016): I commented lines below, otherwise the
+#       (staged and commited git) changes made wouldn't be taken into account.
+#       It would always be the original version (GIT_TAG below!) used
+
+# itk_fetch_module(SmoothingRecursiveYvvGaussianFilter
+#   "GPU and CPU Young & Van Vliet Recursive Gaussian Smoothing Filter: http://hdl.handle.net/10380/3425"
+#   GIT_REPOSITORY ${git_protocol}://github.com/Inria-Asclepios/SmoothingRecursiveYvvGaussianFilter
+#   GIT_TAG 69a6d9569c3a160872ba579f96db789df2ef07e6
+#   )
